b50548f772e9bce4ec0147c3ee15dcfff8c4dc5f
daniel-dieguez <danguez2001@gmail.com^C>
2023-09-27 22:24:25 -0600

    primer repositorio de proyecto

---
+++ src/main/java/com/gymantigua/GymAntiguaApplication.java
@@ -0,0 +1,13 @@
+package com.gymantigua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAntiguaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymAntiguaApplication.class, args);
+    }
+
+}
